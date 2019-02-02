addf686ab45244866d4168626a8b4ee10a9627cc
Tomasz Czekaj <tomasz.czekaj@gmail.com>
2019-02-02 14:13:38 +0100

    Account details added

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,20 @@
 public abstract class Account {
+    private String customerName;
+    private String customerSurname;
+    private String accountNumber;
+    int amount;
+
+
+    public void Deposit(int depositAmount) {
+        amount = amount + depositAmount;
+    }
+
+    public void Withdraw(int withdrawalAmount) {
+        amount = amount - withdrawalAmount;
+    }
+
+    public void Bonus(){
+        amount = amount + 100;
+    }
 
 }
